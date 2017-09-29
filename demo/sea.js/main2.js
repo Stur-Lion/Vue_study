fa6bcd33ac047604eea263f16b85cfa7bfd801a6
chengcheng <cheng.cheng@haoqitech.com.cn>
2017-09-29 14:42:40 +0800

    no message

--- demo/sea.js/main2.js
+++
@@ -1,9 +0,0 @@
-define(function(){
-    var $ = require('jquery')
-    $('#myButton').on('click', function () {
-        var $btn = $(this).button('loading')
-        // business logic...
-        $btn.button('reset')
-    })
-    window.$=window.jQuery = $
-})
--- demo/sea.js/module-1.js
+++
@@ -1,6 +0,0 @@
-define(function(require,exports,module){
-    function num(){
-        console.log(111);
-    }
-    exports.num = num
-})
