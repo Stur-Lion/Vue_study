2734e52dba437b401a23f4980b6348a8b1fa1f03
SJY-PC\SJY <1504468447@qq.com>
2017-10-07 00:33:22 +0800

    提交

--- demo/day 5(router)/demo/src/App.vue
+++ demo/day 5(router)/demo/src/App.vue
@@ -3,12 +3,15 @@
     <img src="./assets/logo.png">
     <div>
       <router-link to='/'>hello</router-link>&nbsp;
-      <router-link to='/hi'>hi</router-link>&nbsp;
+      <!--<router-link to='/hi'>hi</router-link>&nbsp;
       <router-link :to="{name:'hi1',params:{username:'lion',id:'ynn'}}">hi1</router-link>&nbsp;
-      <router-link to='/hi/hi2'>hi2</router-link>
+      <router-link to='/hi/hi2'>hi2</router-link>-->
+      <router-link to='/params/20/lion'>params</router-link>
     </div>
-    <h2>{{ $route.name }}</h2>
+    <!--<h2>{{ $route.name }}</h2>-->
     <router-view></router-view>
+    <!--<router-view name="left" style="float: left;background-color: blue;width: 50%;height: 100px;"></router-view>
+    <router-view name="right" style="float: left;background-color: yellow;width: 50%;height: 100px;"></router-view>-->
   </div>
 </template>
 
--- demo/day 5(router)/demo/src/router/index.js
+++ demo/day 5(router)/demo/src/router/index.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import hi from '@/components/hi'
-import hi1 from '@/components/hi1'
+/*import hi1 from '@/components/hi1'
 import hi2 from '@/components/hi2'
+import Left from '@/components/left'
+import Right from '@/components/right'*/
+import Params from '@/components/params'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'Hello',
       component: Hello
@@ -23,6 +26,30 @@ export default new Router({
         {path:'hi1',name:'hi1',component:hi1},
         {path:'hi2',name:'hi2',component:hi2},
       ]
+    }*/
+    /*{
+      path:'/',
+      name:Hello,
+      component:Hello
+    },
+    {
+      path:'/lion',
+      name:Hello,
+      components:{
+        default :Hello,
+        left:Left,
+        right:Right
+      }
+    }*/
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path:'/params/:id(\\d+)/:title',
+      name:'params',
+      component:Params
     }
   ]
 })
