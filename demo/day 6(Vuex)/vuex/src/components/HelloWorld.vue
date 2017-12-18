0e4133d3fcfcb024627acf878510b54bb613af79
chengcheng <chengcheng@haoqitech.com.cn>
2017-12-18 18:02:38 +0800

    代码提交

---
+++ demo/day 6(Vuex)/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h2>{{ count }}</h2>
+    <p>
+      <button @click="$store.commit('add',change())">+</button>
+      <button @click="$store.commit('reduce')">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  import store from '@/vuex/store';
+  import { mapState } from 'vuex';
+export default {
+  name: 'HelloWorld',
+  data:function () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+  methods:{
+    change:function(){
+      return 'change'
+    }
+  },
+  computed:mapState(['count']),
+  store
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- demo/day 6(Vuex)/vuex/src/router/index.js
+++ demo/day 6(Vuex)/vuex/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import lion from '@/components/lion'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'lion',
-      component: lion
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
